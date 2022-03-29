b083a770826f6a1c3734a776acc1549c06ed6b5a
Vlad Zusko <vladzvu11@gmail.com>
2022-03-29 14:44:46 +0300

    fix: added repositories index

---
+++ repositories/index.js
@@ -0,0 +1,13 @@
+const AuthorsRepository = require('./authors.js');
+const BookTypesRepository = require('./book-types.js');
+const BooksRepository = require('./books.js');
+const UserTypesRepository = require('./user-types.js');
+const UsersRepository = require('./users.js');
+
+module.exports = {
+  AuthorsRepository,
+  BookTypesRepository,
+  BooksRepository,
+  UserTypesRepository,
+  UsersRepository,
+};
